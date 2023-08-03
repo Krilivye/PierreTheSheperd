a9aa0eaec0fbcf336e96d50ee508bf7538c164da
Pierre Bousquié <pierre.bousquie@gmail.com>
2023-08-03 17:35:55 +0200

    first commit

---
+++ src/Main.hx
@@ -0,0 +1,25 @@
+import Player;
+
+final class Main extends hxd.App {
+
+    private var player: Player;
+
+    override function init() {
+        var font = hxd.res.DefaultFont.get();
+        var text = new h2d.HtmlText.HtmlText(font, s2d);
+        font.resizeTo(32);
+        text.text = h2d.HtmlText.defaultFormatText("<font color='#FF0000'>MOVING RECTANGLES</font>");
+        text.x = s2d.width * 0.5;
+
+        this.player = new Player(50, 50, 40, 75);
+        s2d.addChild(this.player);
+    }
+
+    override function update(dt: Float) {
+        this.player.update(dt);
+    }
+
+    static function main() {
+        new Main();
+    }
+ }
